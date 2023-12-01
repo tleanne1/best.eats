a15eef0f1f362cc67ef8494f8c927c07e708b911
tleanne1 <114450881+tleanne1@users.noreply.github.com>
2023-12-01 17:45:06 -0600

    added Navbar content

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+      {/* Left side */}
+      <div className="flex items-center">
+        <div className="cursor-pointer">
+          <AiOutlineMenu size={30} />
+        </div>
+        <h1 className="text-2xl sm:text-2xl lg:text-4xl px-2">
+          Best <span className="font-bold">Eats</span>
+        </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-black text-white rounded-full p-2">Delivery</p>
+          <p className="p-2">Pickup</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
